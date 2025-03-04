176e11c2a68d51fc6a8ab921fc77eb70a491d6e2
Ranjeet Vishwakarma <rv238502@gmail.com>
2025-03-04 19:32:15 +0530

    created the formateDate as a utility function

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -4,3 +4,18 @@ export const formatMessageTime = (date) =>
     minute: "2-digit",
     hour12: true,
   });
+
+export const formatDate = (date) => {
+  const today = new Date();
+  const messageDate = new Date(date);
+  const diffInDays = Math.floor((today - messageDate) / (1000 * 60 * 60 * 24));
+
+  if (diffInDays === 0) return "Today"; // Show "Today" for today's messages
+  if (diffInDays === 1) return "Yesterday";
+  if (diffInDays < 7) {
+    return new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(
+      messageDate
+    );
+  }
+  return messageDate.toLocaleDateString("en-GB").replace(/\//g, "/"); // Format: DD/MM/YY
+};
