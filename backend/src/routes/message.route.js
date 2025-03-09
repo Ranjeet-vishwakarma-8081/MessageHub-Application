6d637a4413c36659349e30654268420011e696aa
Ranjeet Vishwakarma <rv238502@gmail.com>
2025-03-09 21:48:59 +0530

    Added the reset-notification route

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -3,6 +3,7 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 import {
   getMessages,
   getUsersForSidebar,
+  resetNotification,
   sendMessage,
 } from "../controllers/message.controller.js";
 
@@ -11,5 +12,8 @@ const router = express.Router();
 router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages);
 router.post("/send/:id", protectRoute, sendMessage);
+
+// reset notification route
+router.patch("/reset-notification/:id", protectRoute, resetNotification);
 
 export default router;
